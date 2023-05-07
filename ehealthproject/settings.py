af0d403992fb62557a84677ebe5d7b869a289fa4
Daniel Adeboye <adeboyed93@gmail.com>
2023-05-07 16:49:32 +0100

    Update settings.py

--- ehealthproject/settings.py
+++ ehealthproject/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-import os
 import secrets
 from pathlib import Path
 from datetime import timedelta
@@ -110,12 +109,8 @@ WSGI_APPLICATION = 'ehealthproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
